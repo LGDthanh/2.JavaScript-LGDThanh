6a1369adbc6ee0476efeef8b5d38bfe32cdb5713
thanh <thanhnp.1402@gmail.com>
2023-04-26 21:55:59 +0700

    lesson-1

---
+++ Lesson1.js
@@ -0,0 +1,19 @@
+// alert('Hi JavaScript - LGD')
+var fullName =' LGDthanh';
+var age = 21;
+// alert(fullName);
+// alert(age);
+// console.log('dòng log LGD')
+// console.log(fullName)
+// console.warn(fullName);
+// console.error(fullName);
+// confirm('Xac nhan tren 18 tuoi');
+// prompt('Xac nhan tren 18 tuoi');
+// setTimeout(function() {
+//     alert('setTimeout')
+// }, 1000);
+// setInterval(function() {
+//     console.log('Day là log LGD'+ Math.random());
+// }, 1000)
+
+
