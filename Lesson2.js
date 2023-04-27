b52390cc985ae6d1e536d2c68b16fb992896ceea
thanh <thanhnp.1402@gmail.com>
2023-04-27 22:30:23 +0700

    Lesson 3

---
+++ Lesson2.js
@@ -0,0 +1,46 @@
+
+// Toán tử:
+var a = 1 + 2; // 1.Toán tử số học - Arithmetic
+var fullName = 'Thanh Nguyen' // 2. Toán tử gán - Assignment
+var c = 1; var b = 2; if(c>b){ alert('True')}; // 3. Toán tử so sánh - Comparison 
+if(a>0 && b>0){ alert(' a và b > 0')} // 4. Toán tử logic - Logical
+
+// +, -, *, /
+// Lũy thừa: **
+// % chia lấy dư
+// ++, --
+// Prefix & Postfix
+var output1 = ++a;  // Việc 1: +1 ch a, a = a +1 => a=4
+                   // Việc 2: Trả về a sau khi được + 1
+                   
+var output2 = a++;  // Việc 1: `a copy`, `a copy` = 6 
+                    // Việc 2: cộng 1 cho a, a = a +1 => a =4
+                    // việc 3 trả về a copy
+var output3 = a++ - --a; 
+
+// console.log(output);
+// console.log(a);
+// Toán tử gán
+//Ví dụ            Tương đương
+//  x= y;              x= y;
+//  x+=y;              x=x+y;
+//  x*=y;              x=x*y;
+//  x**=y;             x=x**y;
+// Toán tử chuỗi:
+var fullName = 'Thanh';
+var firstName = 'Nguyen';
+var name = ' thanh';
+name = name + ' Nguyễn';
+// console.log(firstName + ' ' + fullName);
+console.log(name)
+//Toán tử so sánh
+// ==
+// !=
+// ><
+// >=
+// <=
+var boolean = a > b;// Boolean true, false 
+// Toán tử logic
+// && - And
+// || - Or
+// ! - Not
---
+++ Lesson3.js
@@ -0,0 +1,21 @@
+// if(){} else{}
+/* Điều kiện sai(false)
+* 0
+* false
+*'' ""
+*undefined
+*NaN
+*null
+Còn lại tất cả là điều kiện đúng(true)
+*/
+/* 1. Kiểu dữ liệu nguyên thủy - Primitive Data
+- Number
+- String
+- Boolean
+- Undefined - var a;
+- Null - npthing
+- Symbol - unique
+2. Dữ liệu phức tạp - Complex Data
+- function
+- object {} - array []
+*/
