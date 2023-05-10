db69d435613f015b8f5d75dacd05c6538629f1f0
Nguyễn Phúc Thanh <125104908+LGDthanh@users.noreply.github.com>
2023-05-10 22:34:27 +0700

    Lesson 11

---
+++ Lesson11.js
@@ -0,0 +1,71 @@
+/*
+    Array methods:
+    ForEach()
+    every()
+    some()
+    find()
+    filter()
+    map()
+    reduce()
+*/
+
+// var n = 5; // Số lượng object bạn muốn tạo
+// var test = [];
+
+// for (var i = 0; i < n; i++) {
+//     var name = prompt("Nhập tên 'name':");
+//     var coin = prompt("Nhập số 'coin':");
+//     var obj = {
+//         id: i + 1,
+//         name: name,
+//         coin: coin
+//     };
+//     test.push(obj);
+// }
+
+// console.log(test);
+
+var courses = [
+    {id: 1, name: 'Thanh', coin: 1},
+    
+    {id: 2, name: 'LGD', coin: 0},
+    
+    {id: 3, name: 'Javascript', coin: 0 },
+    {id: 4, name: 'Python', coin: 0},
+    {id: 2, name: 'LGD', coin: 20},
+
+    {id: 5, name: 'C++', coin: 200}
+]
+
+// courses.forEach(function(courses, index){
+//     console.log(courses, index);
+// })
+
+// var isFree = courses.every(function(course, index){
+//     console.log(index);
+    
+//     return course.coin === 0
+// })
+// console.log(isFree)
+
+// var isFree = courses.some(function(course, index){
+//     console.log(index);
+    
+//     return course.coin === 0
+// })
+// console.log(isFree)
+
+// var isFree = courses.find(function(course, index){
+//     console.log(index);
+    
+//     return course.name === 'LGD'
+// })
+// console.log(isFree)
+
+var listCourses = courses.filter(function(course, index){
+    console.log(index);
+    
+    return course.name === 'LGD'
+})
+console.log(listCourses)
+
--- Lesson9.js
+++ Lesson9.js
@@ -18,11 +18,22 @@ var myInfo = {
     
 } 
  // 1. for - lặp với điều kiện đúng
-
+// break
  for(var i = 1; i<=5; i++){
     //Code
     console.log(i);
+    if(i>=3){
+      break;
+    }
  }
+// continue
+ for(var i = 1; i<=5; i++){
+   //Code
+   console.log(i);
+   if(i%2 !==0){
+     continue;
+   }
+   }
  // 2. for/in - lặp qua key của đối tượng
  // Key của Array là số, Object là string
  for( var key in myInfo){
@@ -30,3 +41,19 @@ var myInfo = {
     console.log(myInfo[key]);
 
  }
+ // 3. for/of - lặp qua value trả ra value
+ for( var value of myInfo){
+   // Code
+
+ }
+// 4. while - lặp khi điều kiện đúng
+var i = 0;
+while(i<10){
+   i++;
+   console.log(i);
+}
+// 5. do/while - lặp ít nhất 1 lần, sau đó lặp khi điều kiện đúng
+do{
+   i++;
+   console.log(i);
+} while(i<10);
